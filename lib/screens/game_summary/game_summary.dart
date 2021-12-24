284631f79acd975017b821c040ca06c37df53d63
Miroslav Mazel <contact.mirek@protonmail.com>
2021-12-24 20:20:41 +0100

    Game summary: minor changes

--- lib/screens/game_summary/game_summary.dart
+++ lib/screens/game_summary/game_summary.dart
@@ -1,3 +1,39 @@
+// This file is part of Parlera.
+//
+// Parlera is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version. As an additional permission under
+// section 7, you are allowed to distribute the software through an app
+// store, even if that store has restrictive terms and conditions that
+// are incompatible with the AGPL, provided that the source is also
+// available under the AGPL with or without this permission through a
+// channel without those restrictive terms and conditions.
+//
+// Parlera is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Parlera.  If not, see <http://www.gnu.org/licenses/>.
+//
+// This file is derived from work covered by the following license notice:
+//
+//   Copyright 2021 Kamil Rykowski, Kamil Lewandowski, and Ewa Osiecka
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -28,7 +64,7 @@ class GameSummaryScreen extends StatelessWidget {
     return List.generate(div2length, (index) {
       //todo turn into gridview, make whole page scrollable
       return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 8),
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         child: Row(
           children: [
             Expanded(
@@ -54,7 +90,7 @@ class GameSummaryScreen extends StatelessWidget {
               child: Column(
                 children: [
                   Padding(
-                    padding: const EdgeInsets.only(bottom: 0, top: 8),
+                    padding: const EdgeInsets.only(bottom: 4, top: 8),
                     child: Text(
                       AppLocalizations.of(context).summaryHeader,
                       // style: Theme.of(context).textTheme,
@@ -68,7 +104,7 @@ class GameSummaryScreen extends StatelessWidget {
                     child: Padding(
                       padding: const EdgeInsets.symmetric(
                         vertical: 8.0,
-                        horizontal: 16.0,
+                        horizontal: 20.0,
                       ),
                       child: Text(
                         model.questionsPassed.length.toString(),
@@ -99,8 +135,8 @@ class GameSummaryScreen extends StatelessWidget {
                   const Padding(
                     padding: EdgeInsets.symmetric(vertical: 16),
                     child: Divider(
-                      indent: 32,
-                      endIndent: 32,
+                      indent: 16,
+                      endIndent: 16,
                     ),
                   ),
                   ElevatedButton.icon(
--- lib/screens/game_summary/widgets/question_item.dart
+++ lib/screens/game_summary/widgets/question_item.dart
@@ -1,3 +1,39 @@
+// This file is part of Parlera.
+//
+// Parlera is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version. As an additional permission under
+// section 7, you are allowed to distribute the software through an app
+// store, even if that store has restrictive terms and conditions that
+// are incompatible with the AGPL, provided that the source is also
+// available under the AGPL with or without this permission through a
+// channel without those restrictive terms and conditions.
+//
+// Parlera is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Parlera.  If not, see <http://www.gnu.org/licenses/>.
+//
+// This file is derived from work covered by the following license notice:
+//
+//   Copyright 2021 Kamil Rykowski, Kamil Lewandowski, and Ewa Osiecka
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
 import 'package:flutter/material.dart';
 import 'package:parlera/helpers/theme.dart';
 import 'package:parlera/models/question.dart';
@@ -15,14 +51,14 @@ class QuestionItem extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
         Container(
-            padding: const EdgeInsets.all(8),
+            padding: const EdgeInsets.all(4),
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(80),
                 color:
                     success ? ThemeHelper.successColor : ThemeHelper.failColor),
             child: Icon(
               success ? Icons.check : Icons.close,
-              size: 20.0,
+              size: 16.0,
               color: Theme.of(context).colorScheme.onSurface,
             )),
         const SizedBox(
