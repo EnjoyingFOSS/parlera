a930fef7824ecaceedaf96083405cdbf832eb2e7
Miroslav Mazel <contact.mirek@protonmail.com>
2023-07-09 15:54:54 +0200

    License header for AnswerGrid

--- lib/screens/game_results/widgets/answer_grid.dart
+++ lib/screens/game_results/widgets/answer_grid.dart
@@ -1,3 +1,23 @@
+// This file is part of Parlera.
+//
+// Parlera is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version. As an additional permission under
+// section 7, you are allowed to distribute the software through an app
+// store, even if that store has restrictive terms and conditions that
+// are incompatible with the AGPL, provided that the source is also
+// available under the AGPL with or without this permission through a
+// channel without those restrictive terms and conditions.
+//
+// Parlera is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with Parlera.  If not, see <http://www.gnu.org/licenses/>.
+
 import 'package:flutter/material.dart';
 import 'package:parlera/models/question.dart';
 
@@ -26,7 +46,6 @@ class AnswerGrid extends StatelessWidget {
             children: List.generate(
               rowCount,
               (rowI) {
-                //TODO turn into gridview, make whole page scrollable
                 if (rowI < rowCount - 1) {
                   return TableRow(
                     children: List.generate(
