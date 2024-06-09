18ac9ae3821e53aafc2e11b3492801c6d1bcf58f
Miroslav Mazel <enjoyingfoss+git@pm.me>
2024-06-09 19:38:04 +0200

    New license header + minor refactors

--- lib/screens/tutorial/tutorial.dart
+++ lib/screens/tutorial/tutorial.dart
@@ -1,14 +1,24 @@
+// Copyright Miroslav Mazel
+//
 // This file is part of Parlera.
 //
 // Parlera is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version. As an additional permission under
-// section 7, you are allowed to distribute the software through an app
-// store, even if that store has restrictive terms and conditions that
-// are incompatible with the AGPL, provided that the source is also
-// available under the AGPL with or without this permission through a
+// (at your option) any later version.
+//
+// As an additional permission under section 7, you are allowed to distribute
+// the software through an app store, even if that store has restrictive terms
+// and conditions that are incompatible with the AGPL, provided that the source
+// is also available under the AGPL with or without this permission through a
 // channel without those restrictive terms and conditions.
+//
+// As a limitation under section 7, all unofficial builds and forks of the app
+// must be clearly labeled as unofficial in the app's name (e.g. "Parlera
+// UNOFFICIAL", never just "Parlera") or use a different name altogether.
+// If any code changes are made, the fork should use a completely different name
+// and app icon. All unofficial builds and forks MUST use a different
+// application ID, in order to not conflict with a potential official release.
 //
 // Parlera is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -30,7 +40,7 @@ import 'package:parlera/store/tutorial.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class TutorialScreen extends StatefulWidget {
-  const TutorialScreen({Key? key}) : super(key: key);
+  const TutorialScreen({super.key});
 
   @override
   State<TutorialScreen> createState() => _TutorialScreenState();
@@ -147,9 +157,9 @@ class _TutorialScreenState extends State<TutorialScreen> {
                       child: PaginationBar(
                           onFinish: () async {
                             await TutorialModel.of(context).setAsWatched();
-                            if (mounted) {
-                              Navigator.popUntil(
-                                  context, ModalRoute.withName('/'));
+                            if (context.mounted) {
+                              Navigator.of(context).popUntil(
+                                  ModalRoute.withName('/'));
                             }
                           },
                           pageCount: pages.length,
--- lib/screens/tutorial/widgets/page_data.dart
+++ lib/screens/tutorial/widgets/page_data.dart
@@ -1,14 +1,24 @@
+// Copyright Miroslav Mazel
+//
 // This file is part of Parlera.
 //
 // Parlera is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version. As an additional permission under
-// section 7, you are allowed to distribute the software through an app
-// store, even if that store has restrictive terms and conditions that
-// are incompatible with the AGPL, provided that the source is also
-// available under the AGPL with or without this permission through a
+// (at your option) any later version.
+//
+// As an additional permission under section 7, you are allowed to distribute
+// the software through an app store, even if that store has restrictive terms
+// and conditions that are incompatible with the AGPL, provided that the source
+// is also available under the AGPL with or without this permission through a
 // channel without those restrictive terms and conditions.
+//
+// As a limitation under section 7, all unofficial builds and forks of the app
+// must be clearly labeled as unofficial in the app's name (e.g. "Parlera
+// UNOFFICIAL", never just "Parlera") or use a different name altogether.
+// If any code changes are made, the fork should use a completely different name
+// and app icon. All unofficial builds and forks MUST use a different
+// application ID, in order to not conflict with a potential official release.
 //
 // Parlera is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -31,8 +41,7 @@ class TutorialPage extends StatelessWidget {
       required this.title,
       required this.description,
       required this.imagePath,
-      Key? key})
-      : super(key: key);
+      super.key});
 
   @override
   Widget build(BuildContext context) {
