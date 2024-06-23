626d5141882b7235845aefe5614bc562f70ec85b
Miroslav Mazel <enjoyingfoss+git@pm.me>
2024-06-23 16:58:48 +0200

    Add license enum

---
+++ lib/enums/license.g.dart
@@ -0,0 +1,14 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'license.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+const _$LicenseEnumMap = {
+  License.copyrighted: 'copyrighted',
+  License.ccBySa4: 'ccBySa4',
+  License.ccBy4: 'ccBy4',
+  License.ccZero: 'ccZero',
+};
